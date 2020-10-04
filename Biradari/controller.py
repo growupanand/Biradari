4befa7044658d20f3b4af4a321901f950a299a89
GrowUpAnand <growupanand@gmail.com>
2020-10-04 17:27:28 +0530

    testing mongo atlas

--- Biradari/controller.py
+++ Biradari/controller.py
@@ -4,8 +4,10 @@ from bson import ObjectId
 import datetime
 
 
-client = pymongo.MongoClient("mongodb://localhost",27017)
-db = client['Biradari_database']
+#client = pymongo.MongoClient("mongodb://localhost",27017)
+
+client = pymongo.MongoClient("mongodb+srv://test:12345@cluster0.4luf0.mongodb.net/test?retryWrites=true&w=majority")
+db = client.test
 
 user_collection = db['users']
 request_collection = db['requests']
