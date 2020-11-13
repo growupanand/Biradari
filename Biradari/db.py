ac7be0034d8e21362e60243470d865ca4ca8f63f
GrowUpAnand <growupanand@gmail.com>
2020-11-13 17:20:30 +0530

    fix db

--- Biradari/db.py
+++ Biradari/db.py
@@ -1,9 +1,9 @@
 import pymongo
 
-client = pymongo.MongoClient("mongodb://localhost",27017)
+#client = pymongo.MongoClient("mongodb://localhost",27017)
 
 client = pymongo.MongoClient("mongodb+srv://test:12345@cluster0.4luf0.mongodb.net/test?retryWrites=true&w=majority")
-#db = client['test']
+db = client['test']
 
 user_collection = db['users']
 request_collection = db['requests']
