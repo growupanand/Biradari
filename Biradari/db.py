02bbdf061ad9be9ead8e25c92cc1c5ecd64017c9
GrowUpAnand <growupanand@gmail.com>
2020-11-13 16:39:07 +0530

    Refresh project with new look

---
+++ Biradari/db.py
@@ -0,0 +1,10 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost",27017)
+
+#client = pymongo.MongoClient("mongodb+srv://test:12345@cluster0.4luf0.mongodb.net/test?retryWrites=true&w=majority")
+db = client['test']
+
+user_collection = db['users']
+request_collection = db['requests']
+post_collection = db['posts']
--- Biradari/views.py
+++ Biradari/views.py
@@ -1,66 +1,99 @@
-from Biradari import app, controller
+from Biradari import app, controller, stringtotimestamp
 from flask import render_template, session
 from datetime import datetime
-
+from Biradari.user import user
 
 
 @app.route('/')
-def index():
+def index_page():
     if session.get('logged_in') == True:
-        if not 'father' in session.get('login_user') or not 'mother' in session.get('login_user'):
-            return render_template('finish.html', user=session.get('login_user'))
-        return render_template('home.html')
+        if not 'father' in session.get('user') or not 'mother' in session.get('user'):
+            return render_template('finish.html', user=session.get('user'))
+        user_data = user(session['user']['username'])
+        posts = user_data.get_posts(limit=10, get_wall_posts=True)
+        first_timestamp = posts[0]['timestamp'] if len(posts)>0 else None
+        last_timestamp = posts[len(posts)-1]['timestamp'] if len(posts)>0 else None
+        for i in range(len(posts)):
+            timestamp = stringtotimestamp(posts[i]['timestamp'])
+            posts[i]['timestamp'] = timestamp.strftime('%d/%B/%Y %H:%M')
+        return render_template('home.html', posts=posts, first_timestamp=first_timestamp, last_timestamp=last_timestamp)
     else:
-        return render_template('guest.html')
+        return render_template('login.html')
 
 
 @app.route('/register')
-def register():
+def register_page():
     return render_template('register.html')
 
+
 @app.route('/login')
-def login():
+def login_page():
     return render_template('login.html')
 
-@app.route('/add_father')
-def view_addfather():
-    return render_template('add_father.html')
 
-@app.route('/add_mother')
-def view_addmother():
-    return render_template('add_mother.html')
+@app.route('/logout')
+def logout_page():
+    session.clear()
+    return render_template('login.html')
 
 
 @app.route('/profile/<username>')
 @app.route('/profile')
-def profile(username = None):
-    if username == None or username == session['login_user']['username']:
-        user = session['login_user']
-        page = 'user_profile.html'
+def profile_page(username = None):
+    is_biradari = False
+    #check if logged in user visited page then show his profile page
+    if username == None or username == session['user']['username']:
+        user_data = user(session['user']['username'])
+        profile_page = 'user_profile.html'
     else:
-        user = controller.get_profile(username)
-        if user == False:
-            return 'user not found'
-        page = 'view_profile.html'
+        #show other user profile page
+        user_data = user(username)
+        #check if other user is exist
+        if user_data == False:
+            return 'User not found'
+        profile_page = 'view_profile.html'
+        #check if other user is in biradari list
+        if username in user(session['user']['username']).get_biradari():
+            is_biradari = True
+    user_profile = user_data.profile()
+    user_profile['dob'] = datetime.strptime(user_profile['dob'], '%Y-%m-%d').strftime('%d-%m-%Y')
+    biradari_list = []
+    for i in user_data.get_biradari():
+        biradari_list.append({'username':i,'full_name':user(i).profile()['full_name']})
+    return render_template(profile_page, user=user_profile, biradari_list=biradari_list, is_biradari=is_biradari)
 
-    user['dob'] = datetime.strptime(user['dob'], '%Y-%m-%d').strftime('%d-%m-%Y')
-    return render_template(page, user=user)
 
 @app.route('/edit_profile')
-def edit_profile():
-    user = session['login_user']
-    return render_template('edit_profile.html', user = user)
+def edit_profile_page():
+    user_profile = user(session['user']['username']).profile()
+    return render_template('edit_profile.html', user = user_profile)
 
-@app.route('/biradari/<username>')
+
 @app.route('/biradari')
-def biradari(username = None):
-    if username == None or username == session['login_user']['username']:
-        user = controller.get_profile(session['login_user']['username'])
-    else:
-        user = controller.get_profile(username)
+def biradari_page():
+    biradari_list = []
+    user_data = user(session['user']['username'])
+    for username in user_data.get_biradari():
+        biradari_list.append({
+            'username' : username,
+            'full_name' : user(username).profile()['full_name']
+        })
+    return render_template('biradari.html', user=user_data, biradari_list=biradari_list)
 
-    user['biradari'] =controller.get_biradari(user['username'])
-    return render_template('biradari.html', user = user)
+
+@app.route('/search')
+def search_page():
+    return render_template('search.html')
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/edit_biradari')
 def edit_biradari():
